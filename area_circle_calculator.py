24e823eb3fa6f2de43b36370d62424b7deb5403d
samanta baq <jerilsamanta101@gmail.com>
2026-02-17 19:41:56 -0500

    Create area_circle_calculator.py
    #area calculator
    -ask the radius of the circle
    -calculates the area
    -shows the area

---
+++ area_circle_calculator.py
@@ -0,0 +1,6 @@
+import math
+
+radius = float(input('enter the radius of the circle:')) #ask to enter the radius of the circle
+print(radius) #show the radius
+area = math.pi * math.pow(radius, 2) #area=pi*r^2
+print('the area of the circle is:',round(area)) #round the area
